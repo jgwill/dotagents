624e519ff3d8606d959dfe3cea8d9d741dbc3554
Gerico1007 <gerico@jgwill.com>
2026-08-18 16:34:45 -0400

    draft jgwill/dotagents#27 by @jgwill on gmusic@eury 2608181634

---
+++ scripts/_env.sh
@@ -0,0 +1,12 @@
+
+herdr_space_renamed="orpheus-ep333-atelier-JAMAI-260818-RISE"
+herdr_space_original_name="atelier JAMAI — suite"
+session_jerry_origin_id="1937aa47-767f-4543-8cdc-257364ae2c52"
+session_jamai_william_observer_opus003_metal_id="7cd9bb52-eab9-4dfb-861b-680bcfa1df17"
+session_jamai_william_observer_opus003_herdr_pane="observe-william-forking-ATELIER-jamai-demain-260816-71bbe83b--JAMAI--opus003-metal-music-hahaha"
+session_jamai_william_observer_opus003_metal_name="jamai-opus003-observe-william-forking-ATELIER-jamai-demain-260816-71bbe83b"
+session_fork_ava002_id="71bbe83b-8963-4635-b8a2-40bcffbb3aff"
+session_fork_ava002_name="ava002-fork-pane--ATELIER-jamai-demain--creator-of-opus014-by-willifor-rise-framework"
+session_fork_ava002_herdr_pane="fork-pane--ATELIER-jamai-demain--creator-of-opus014-by-william-for-rise-framework--AUREON-ava002-003"
+
+
